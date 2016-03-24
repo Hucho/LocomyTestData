cfdd0f1a1bb0781cd76ac411a5f3c6cd2e9f2fd2
Hucho <matthias.abele@outlook.com>
2016-03-24 22:23:52 +0100

    amazon/mongo working

---
+++ config/credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+  accessKeyId: "AKIAJLOQBRCLWOA5JXAQ",
+  secretAccessKey: "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",
+  associateId_de: 'wwwlocomyde-21',
+  associateId_en: 'wwwlocomycom-21'
+}
--- server.js
+++ server.js
@@ -5,19 +5,10 @@ var express = require ("express");
 var app = express();
 var bodyParser = require("body-Parser");
 var methodOverride = require("method-override");
-var amazon = require('amazon-product-api');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var models = require('./app/models/locomyDB.js')(mongoose, Schema);
-
-//create Amazon client
-var client = amazon.createClient({
-
-	aws: "aws ID",
-	awsSecret: "aws Secret",
-	awsTag: 'aws Tag'
-
-});
+var keys = require ('./config/credentials.js')
 
 //set our port =================================================
 var port = process.env.PORT || 3000;
@@ -25,48 +16,99 @@ var port = process.env.PORT || 3000;
 //set mongoDB connection
 mongoose.connect('mongodb://localhost/locomyDB');
 
+//require util and apac
+var util = require('util'),
+    OperationHelper = require('apac').OperationHelper;
+
+//generate Operationhelper with ids
+var opHelper = new OperationHelper({
+  awsId: keys.accessKeyId,
+  awsSecret: keys.secretAccessKey,
+  assocId: keys.associateId_en
+});
+
+//genreate request and print to console and store in mongoDB
+
+	opHelper.execute('ItemSearch', {
+	  'SearchIndex': 'SportingGoods',
+	  'Title': 'Soccer Ball',
+	  'KeyWords': 'Soccer',
+	  'ResponseGroup': 'ItemAttributes',
+	  'sort': 'relevance'
+
+	}, function(err, results) {
+		
+		if(err) {console.log(err);}
+
+		else {
+
+			console.log(typeof(results));
+			
+			for (var numOfitems = 0; numOfitems < results.ItemSearchResponse.Items[0].Item.length; numOfitems ++)	{
+			 
+			 console.log(results.ItemSearchResponse.Items[0].Item[numOfitems].ItemAttributes[0].Title);
+			 var newProduct = new models.products({
+
+			 		title: results.ItemSearchResponse.Items[0].Item[numOfitems].ItemAttributes[0].Title
+			 });
+
+			 newProduct.save(function(err){
+
+			if (err) {console.log(err);}
+			else {console.log(newProduct);}
+
+			});
+
+
+			}
+
+		}
+		
+	});
+
+//send request to browser
 
 //enter new rating
-var rating = new models.product_ratings({
+// var rating = new models.product_ratings({
 
-		id: 1,
-		product_id: 5,
-		rate: 3,
-		description: 'A great product!'
+// 		id: 1,
+// 		product_id: 5,
+// 		rate: 3,
+// 		description: 'A great product!'
 
-});
+// });
 
-rating.save(function(err){
+// rating.save(function(err){
 
-	if (err) {console.log(err);}
-	else {console.log(rating);}
+// 	if (err) {console.log(err);}
+// 	else {console.log(rating);}
 
-});
+// });
 
 //entner new user
- var newUser = new models.users ({
+//  var newUser = new models.users ({
 
- 	id: 007,
-	username: '00-Agent',
-	password: '007',
-	name: 'James Bond',
-	age: 45,
-	sex: 0,
-	mobile: '+41 000 007',
-	address: 'Downingstreet No. 10, London',
-	is_provider: 0,
-	is_admin_provider: 0,
-	jid: 001,
-	user_location: [{x: 111, y: 222}]
+//  	id: 007,
+// 	username: '00-Agent',
+// 	password: '007',
+// 	name: 'James Bond',
+// 	age: 45,
+// 	sex: 0,
+// 	mobile: '+41 000 007',
+// 	address: 'Downingstreet No. 10, London',
+// 	is_provider: 0,
+// 	is_admin_provider: 0,
+// 	jid: 001,
+// 	user_location: [{x: 111, y: 222}]
 
- });
+//  });
 
-newUser.save(function(err){
+// newUser.save(function(err){
 
-	if (err) {console.log(err);}
-	else {console.log(newUser);}
+// 	if (err) {console.log(err);}
+// 	else {console.log(newUser);}
 
-});
+// });
 
 
 //get all data/stuff of the body (POST) parameters
@@ -86,6 +128,7 @@ app.use(methodOverride("X-HTTP-Method-Override"));
 app.use(express.static(__dirname + '/public'));
 
 //routes ======================================================
+
 require('./app/routes')(app); //configure routes
 
 //start app ===================================================
