89f9310701874e56636141aa3dda4b617328b053
Hucho <matthias.abele@outlook.com>
2016-04-02 23:17:10 +0200

    Restructured some code

--- app/qryHandler.js
+++ app/qryHandler.js
@@ -3,45 +3,53 @@
 //require array from qrybuilder function
 var OperationHelper = require('apac').OperationHelper;
 var Credentials = require('../config/credentials');
+//requrie function which builds the Querry Array
 var QueryBuilder = require('../app/qrybuilder');
+//require mongoDB stuff
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var models = require('../app/models/locomyDB.js')(mongoose, Schema);
+var app = require('../server.js');
 
-//generate new credetials
+//set mongoDB connection
+mongoose.connect('mongodb://localhost/locomyDB');
+mongoose.connection.on('error', console.error.bind('connection error'));
+mongoose.connection.once('open', function(){
+	console.log("Connection to MongoDB successfully established!")
+})
+
+//generate new credetials for connecting the Amazon API
 var cred = new Credentials('EN');
 
 //generate new OperationsHelper for Amazon Product API
 var opHelper = new OperationHelper(cred);
 
-//genreate request and print to console and store in mongoDB
+//genreate Array of Amazon API requests
 var querryArray = [];
 querryArray = QueryBuilder();
 
 function saveData(results){
-	if(results.ItemSearchResponse.Items[0].TotalResults[0] == '0'){
-		console.log("Kein Ergebnis");
-	}
-	else {
-		for(var i = 0; i < results.ItemSearchResponse.Items[0].Item.length; i++ ){
-			var newItem = new models.products({
-				id: results.ItemSearchResponse.Items[0].Item[i].ASIN[0],
-				title: noTitle (results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Title),
-				description: undefinedField (results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Feature),
-				image_link: noPictureHandler (results.ItemSearchResponse.Items[0].Item[i].MediumImage),
-				//brand = manufacturere field from Amazon
-				brand: noManufacHandler (results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Manufacturer),
-				price: undefinedPrices (results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].ListPrice)
-			})
-			newItem.save(function(err,res){
-				if(err) console.log(err)
-					console.log(res)
-			});
-					
+		if(results.ItemSearchResponse.Items[0].TotalResults[0] == '0'){
+			console.log("Kein Ergebnis");
 		}
-	}
-}
-
+		else {
+			for(var i = 0; i < results.ItemSearchResponse.Items[0].Item.length; i++ ){
+				var newItem = new models.products({
+					id: results.ItemSearchResponse.Items[0].Item[i].ASIN[0],
+					title: noTitle (results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Title),
+					description: undefinedField (results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Feature),
+					image_link: noPictureHandler (results.ItemSearchResponse.Items[0].Item[i].MediumImage),
+					//brand = manufacturere field from Amazon
+					brand: noManufacHandler (results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Manufacturer),
+					price: undefinedPrices (results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].ListPrice)
+					})
+					newItem.save(function(err,res){
+					if(err) console.log(err)
+						console.log(res)
+				});
+						
+			}
+		}}
 
 //batch query all requests from the array and then the result to mongoDB; after do a timeout...
 function runQueries(){
@@ -49,15 +57,16 @@ function runQueries(){
 	if(query){
 		makeRequest(query).then(function(results){
 				console.log(query)
+				/*here is the entry point for the saveData function, which writes to MongoDB
+				in my case*/
 				saveData(results);
 			setTimeout(function(){
 				runQueries();
-			}, 1500);
+			}, 1001);
 			;
 			})
 		}
 	}
-
 
 //make a request and resolve the result
 function makeRequest(query){
@@ -67,8 +76,15 @@ function makeRequest(query){
 			else {resolve(results)}
 			})
 	})
-}
+	}
 
+//error handling
+
+//handle problems with undefined title
+function noTitle(titleLink){
+	if(titleLink == undefined){return '';}
+	else{ return titleLink[0]; }
+	}
 //handle problem with undefined item properties
 function undefinedField (field){
 		var value = String;
@@ -77,11 +93,10 @@ function undefinedField (field){
 		 			else if (field.constructor == Object) {value = field;}
 			 		return value;
 				 	}
-
 //handle problems with undefined prices
 function undefinedPrices(price){
-		if(price == undefined) {return '';}
-		else {return price[0].Amount[0];}
+		if(price == undefined) {return ''}
+		else {return price[0].Amount[0]}
 		}
 //handle problems for products without a picture
 function noPictureHandler(picturelink){
@@ -93,15 +108,5 @@ function noManufacHandler (manufLink){
 		if(manufLink == undefined){return "";}
 		else{return manufLink[0];}
 		}
-
-runQueries();
-
-//error handling
-
-//handle problems with undefined title
-function noTitle(titleLink){
-if(titleLink == undefined){return '';}
-else{ return titleLink[0]; }
-}
 
 module.exports = runQueries;
--- server.js
+++ server.js
@@ -5,144 +5,29 @@ var express = require ("express");
 var app = express();
 var bodyParser = require("body-Parser");
 var methodOverride = require("method-override");
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 var util = require('util');
 var runQueries = require('./app/qryHandler');
-var OperationHelper = require('apac').OperationHelper;
-var Credentials = require('./config/credentials');
 
-//set our port =================================================
-var port = process.env.PORT || 3000;
+//rund Queries against Amazon API and save results
+//to MongoDB
+runQueries();
 
-//set mongoDB connection
-mongoose.connect('mongodb://localhost/locomyDB');
-mongoose.connection.on('error', console.error.bind('connection error'));
-mongoose.connection.once('open', function(){
-	console.log("Connection to MongoDB successfully established!")
-})
+function resToBrowser(){
 
-var cred = new Credentials('EN');
-var opHelper = new OperationHelper(cred);
-//send request to browser
+	app.get('/', function(req,res){
 
-app.get('/Error', function(req, res){
+		console.log("Products are requested!");
+		/*model may not be defined due to re-compiling problem
+		with runQueries function*/
+		models.products.find(function(err, docs){
+			console.log(docs);
+			res.json(docs);
+		});
 
-	opHelper.execute ('ItemSearch',
-	 {'SearchIndex': 'Toys',
-	  'Title': 'Lego',
-	  'KeyWords': 'Castle',
-	  'MinimumPrice': '10000',
-	  'MaximumPrice': '1000000',
-	  'ResponseGroup': 'ItemAttributes, Images, BrowseNodes',
-	  'sort': 'relevance'
-		}, function(err, results){
-			
-			if(err) {console.log(err);}
-			else {
-				res.json(results);
-				console.log("I received "+ results.ItemSearchResponse.Items[0].Item.length + " items from Amazon.com!");
-				}
-			}
-		);
-
-	});
-
-function runQueries (querryArray){
-
-	for(var i = 2; i < 3; i++){
-
-	opHelper.execute ('ItemSearch', querryArray[i]
-
-	, function(err, results) {
-		
-		if(err) {console.log(err);}
-
-		else {	console.log(querryArray[i]);
-				for (var numOfitems = 0; numOfitems < results.ItemSearchResponse.Items[0].Item.length; numOfitems ++) {
-				 	//handle problem with undefined item properties
-					function undefinedField (field){
-
-						var value = String;
-
-				 			if (field == undefined) {value ="";}
-
-				 			else if (field.constructor == Array) {value = field.join(',');}
-
-				 			else if (field.constructor == Object) {value = field;}
-
-				 		return value;
-				 	}
-					//handle problems with undefined prices
-					function undefinedPrices(price){
-						if(price == undefined) {return '';}
-						else {return price[0].Amount[0];}
-					}
-					//handle problems for products without a picture
-					function noPictureHandler(picturelink){
-						if(picturelink == undefined) {return "";}
-						else {return picturelink[0].URL[0];}s
-					}
-					//handle problems for products without manufacturer
-					function noManufacHandler (manufLink){
-						if(manufLink == undefined){return "";}
-						else{return manufLink[0];}
-					}
-					//handle problems with undefined title
-					function noTitle(titleLink){
-						if(titleLink == undefined){return '';}
-						else{ return titleLink[0]; }
-					}
-
-					//map http request to mongoose model	
-				 	var newProduct = new models.products({
-				 		id: results.ItemSearchResponse.Items[0].Item[numOfitems].ASIN[0],
-				 		//categroy_id = first order browsenodeId from Amazon
-				 		category_id: undefinedField(results.ItemSearchResponse.Items[0].Item[numOfitems].BrowseNodes[0].BrowseNode[0].BrowseNodeId[0]),
-				 		title: noTitle (results.ItemSearchResponse.Items[0].Item[numOfitems].ItemAttributes[0].Title),
-				 		description: undefinedField (results.ItemSearchResponse.Items[0].Item[numOfitems].ItemAttributes[0].Feature),
-				 		image_link: noPictureHandler (results.ItemSearchResponse.Items[0].Item[numOfitems].MediumImage),
-				 		//brand = manufacturere field from Amazon
-				 		brand: noManufacHandler (results.ItemSearchResponse.Items[0].Item[numOfitems].ItemAttributes[0].Manufacturer),
-				 		price: undefinedPrices (results.ItemSearchResponse.Items[0].Item[numOfitems].ItemAttributes[0].ListPrice)
-				 	});
-
-					 newProduct.save(function(err){
-					 	if (err) {console.log(err);}
-					 });
-					 console.log(newProduct);
-		}
-
-
-			};
-		
-	});
-  }
-
+		});
 }
 
-//send mongo entries to browser with client side
-app.get('/products', function(req,res){
-
-	console.log("Products are requested!");
-	models.products.find(function(err, docs){
-		console.log(docs);
-		res.json(docs);
-	});
-
-
-	});
-
-app.get('/Json', function(req,res){
-
-	console.log("Products are requested!");
-	models.products.find(function(err, docs){
-		console.log(docs);
-		res.json(docs);
-	});
-
-
-	});
+resToBrowser();
 
 //parse application/json
 app.use(bodyParser.json());
@@ -159,11 +44,12 @@ app.use(methodOverride("X-HTTP-Method-Override"));
 //set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public'));
 
-
 //routes ======================================================
 
 //require('./app/routes')(app); //configure routes
 
+//set our port =================================================
+var port = process.env.PORT || 3000;
 //start app ===================================================
 //startup app at http://localhost: 3000
 app.listen(port);
